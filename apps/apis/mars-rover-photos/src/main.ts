a6179b02598c2099dc6229384875d57d336f9f15
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-02-06 13:45:33 +0100

    mrp api: expose rate limit

--- apps/apis/mars-rover-photos/src/main.ts
+++ apps/apis/mars-rover-photos/src/main.ts
@@ -1,20 +1,20 @@
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 const updateQueryStringParameter = (
   path: string,
   key: string,
   value: string
 ) => {
-  const re = new RegExp('([?&])' + key + '=.*?(&|$)', 'i');
-  const separator = path.indexOf('?') !== -1 ? '&' : '?';
+  const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  const separator = path.indexOf("?") !== -1 ? "&" : "?";
   if (path.match(re)) {
-    return path.replace(re, '$1' + key + '=' + value + '$2');
+    return path.replace(re, "$1" + key + "=" + value + "$2");
   } else {
-    return path + separator + key + '=' + value;
+    return path + separator + key + "=" + value;
   }
 };
 
@@ -22,12 +22,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3333;
   app.use(
-    '/api',
+    "/api",
     createProxyMiddleware({
       target: environment.target,
       changeOrigin: true,
       pathRewrite: (path) =>
-        updateQueryStringParameter(path, 'api_key', process.env.API_KEY),
+        updateQueryStringParameter(path, "api_key", process.env.API_KEY),
+      onProxyRes: (res) => {
+        res.headers["access-control-expose-headers"] =
+          "x-ratelimit-limit,x-ratelimit-remaining";
+        return res;
+      },
     })
   );
   if (!environment.production)
