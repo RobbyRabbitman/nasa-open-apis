e31c05cfc1f0da91a270567b82551b4f5724f260
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-02-06 20:00:00 +0100

    mrp app: removed gen code, added setup

--- apps/web/mars-rover-photos/src/app/app.module.ts
+++ apps/web/mars-rover-photos/src/app/app.module.ts
@@ -1,11 +1,22 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { ApiRateLimitDataModule } from "@nasa-open-apis/web/api-rate-limit/api-rate-limit-data";
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+import { NgxsModule } from "@ngxs/store";
+import { environment } from "../environments/environment";
 import { AppComponent } from "./app.component";
-import { NxWelcomeComponent } from "./nx-welcome.component";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    // ngxs
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+    ApiRateLimitDataModule.init(environment.api),
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
