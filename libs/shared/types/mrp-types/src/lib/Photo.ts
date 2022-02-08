2f0c386e32cdf9752a94919b21eb5907204fcf95
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-02-08 21:28:18 +0100

    mrp types: photo corrected

--- libs/shared/types/mrp-types/src/lib/Photo.ts
+++ libs/shared/types/mrp-types/src/lib/Photo.ts
@@ -1,11 +1,15 @@
 import { Camera } from "./Camera";
 import { Rover } from "./Rover";
 
+export type PhotoRoverType = Pick<
+  Rover,
+  "id" | "name" | "landing_date" | "launch_date" | "status"
+>;
 export interface Photo {
   id: number;
   sol: number;
   earth_date: string;
   img_src: string;
-  rover: Rover;
+  rover: PhotoRoverType;
   camera: Camera;
 }
