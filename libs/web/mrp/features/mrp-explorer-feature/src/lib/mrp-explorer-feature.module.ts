a2098e8d215b6d3d7a828286f3f21d7dafc9bc37
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-02-12 16:55:20 +0100

    mrp: explorer: rovers pages set up

--- libs/web/mrp/features/mrp-explorer-feature/src/lib/mrp-explorer-feature.module.ts
+++ libs/web/mrp/features/mrp-explorer-feature/src/lib/mrp-explorer-feature.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MrpUiModule } from "@nasa-open-apis/web/mrp/mrp-ui";
 import { ExplorerComponent } from "./pages/explorer/explorer.component";
 import { RoversComponent } from "./pages/rovers/rovers.component";
 import { MrpExplorerRoutingModule } from "./routes/mrp-explorer-routing.module";
-
 @NgModule({
   declarations: [ExplorerComponent, RoversComponent],
-  imports: [CommonModule, MrpExplorerRoutingModule],
+  imports: [CommonModule, MrpUiModule, MrpExplorerRoutingModule],
 })
 export class MrpExplorerFeatureModule {}
