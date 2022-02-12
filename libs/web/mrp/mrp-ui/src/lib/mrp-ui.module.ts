4feb6b1c0db38c4421d11440a14fb643991e45fe
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-02-12 15:33:15 +0100

    mrp ui: card set up

--- libs/web/mrp/mrp-ui/src/lib/mrp-ui.module.ts
+++ libs/web/mrp/mrp-ui/src/lib/mrp-ui.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
 import { RoverCardComponent } from "./components/rover-card/rover-card.component";
-
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatListModule, MatIconModule],
   declarations: [RoverCardComponent],
   exports: [RoverCardComponent],
 })
