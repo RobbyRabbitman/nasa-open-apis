a65f085c38d93aa6a50613a680ec61d4b2d6105c
RobbyRabbitman <robby.rabbitman@gmail.com>
2022-03-05 20:28:22 +0100

    neo: estimated diameter

--- libs/web/neo/features/neo-feature/src/lib/neo-feature.module.ts
+++ libs/web/neo/features/neo-feature/src/lib/neo-feature.module.ts
@@ -1,10 +1,21 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { NeoUiModule } from "@nasa-open-apis/web/neo/neo-ui";
 import { NeoComponent } from "./pages/neo/neo.component";
 import { NeoRoutingModule } from "./routes/neo-browse-routing.module";
 
 @NgModule({
-  imports: [CommonModule, NeoRoutingModule],
+  imports: [
+    CommonModule,
+    NeoRoutingModule,
+    NeoUiModule,
+    MatListModule,
+    MatCardModule,
+    MatIconModule,
+  ],
   declarations: [NeoComponent],
 })
 export class NeoFeatureModule {}
